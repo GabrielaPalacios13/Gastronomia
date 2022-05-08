fa3626e934117ffcb1005e7952ee1b49672c8d8f
GabrielaPalacios13 <104385932+GabrielaPalacios13@users.noreply.github.com>
2022-05-08 16:19:43 -0500

    html

---
+++ html.jsx
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Pagina de gastronomia</title>
+</head>
+  <link rel="stylesheet" type="text/css" href="./gastro.css">
+ <body>
+<a href="/tacos.html">INICIO1</a>
+<a href="/comida.html">INICIO2</a>
+<a href="/plato mexicano.html">INICIO3</a>
+<a href="/mole.html">INICIO4</a>
+<div class="cuadro">
+<h1>INTEGRANTES:</h1>
+<li>Nayeli Gitzel Perez Mendez</li>
+<li>Arianna Yanetzi Vicencio Ramirez</li>
+<li>Gabriela Palacios Lopez</li>
+<li>Thaniell Gabriel Castillo</li>
+<div class="cuadro2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7s3AYDdrAoMBYyITvtiRYI-POZecypuwKhw&usqp=CAU"></div>
+<div class="cuadro3">Face: Darío De la cruz
+</div>
+<div class="cuadro4">Soul Knight:Soul Knight es un 'roguelite' abiertamente inspirado en 'Enter the Gungeon', en el que los jugadores podrán adentrarse en las profundidades de una mazmorra llena de peligros.</div>
+
+</div>
+</body>
+</html>
